bdb3666899b537ae68f372d85824b637971ebbcb
IsFariza <fariza.ismag@gmail.com>
2026-02-08 02:04:24 +0500

    fix: only female gender

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
     },
     gender: {
         type: String, 
-        enum: ['male', 'female'],
+        enum: ['female'],
         required: true
     },
     email: {
