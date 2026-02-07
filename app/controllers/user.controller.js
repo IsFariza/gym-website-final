04750226b2b10c35c0daa1d0bc3687089b707f3d
IsFariza <fariza.ismag@gmail.com>
2026-02-08 04:03:18 +0500

    feat: add user controller and routes

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,50 @@
+const User = require("../models/user.model");
+
+exports.getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password"); 
+        if (!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json({ message:"Error fetching profile"});
+    }
+};
+
+exports.updateUserProfile = async (req, res) => {
+    try {
+        const { username, email, phoneNumber } = req.body;
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user.id,
+            { username, email, phoneNumber },
+            { new: true, runValidators: true }
+        ).select("-password");
+
+        res.status(200).json({ message: "Profile updated successfull", updatedUser });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+exports.findAllUsers = async (req, res) => {
+    try {
+        const users = await User.find().select("-password");
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({message: "Error fetching users"});
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+        res.status(200).json({message: "User deleted by admin"});
+    } catch (err) {
+        res.status(500).json({message: "Could not delete user"});
+    }
+};
