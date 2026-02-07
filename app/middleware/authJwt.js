f3c986545837193213f9efc4914c6cbc7ca0eb9c
IsFariza <fariza.ismag@gmail.com>
2026-02-08 02:06:26 +0500

    feat: add authJwt

---
+++ app/middleware/authJwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/auth.config');
+
+const verifyToken = (req, res, next) => {
+    let token = req.headers['authorization'];
+    if (!token) 
+        return res.status(403).json({message: 'No token'});
+
+
+    try {
+        const decoded = jwt.verify(token, config.secret);
+        req.user = decoded; 
+        next();
+    } catch (error) {
+        return res.status(401).json({message: 'Unauthorized: invalid token'});
+    }
+};
+
+module.exports = { verifyToken };
