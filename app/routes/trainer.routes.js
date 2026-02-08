bea9ad31313ff68620582a0070ce06c0796d59e5
IsFariza <fariza.ismag@gmail.com>
2026-02-09 01:02:02 +0500

    feat: update trainer

--- app/routes/trainer.routes.js
+++ app/routes/trainer.routes.js
@@ -1,10 +1,12 @@
 const trainerController = require("../controllers/trainer.controller");
 const { verifyToken } = require("../middleware/authJwt");
 const { isAdmin } = require("../middleware/verifyRole");
+const {isTrainer} = require("../middleware/verifyRole")
 
 module.exports = function(app) {
     app.get("/api/trainers", trainerController.getAllTrainers);
-    app.get("/api/trainers/:id", trainerController.getTrainerById);
+    app.get("/api/trainers/:id", trainerController.getTrainerById)
+    app.get("/api/trainer/my-workouts", [verifyToken, isTrainer], trainerController.getTrainerWorkouts)
     app.put("/api/admin/trainers/:id", [verifyToken, isAdmin], trainerController.updateTrainer)
     app.delete("/api/admin/trainers/:id", [verifyToken, isAdmin], trainerController.deleteTrainer)
 };
